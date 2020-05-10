4adfcfa846c52b7bc75d3c75166a4f5c5ddfe3a8
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-10 20:32:32 +0530

    ConnectedComponents

--- BFS.cpp
+++ BFS.cpp
@@ -42,8 +42,10 @@ int main(int argc, char const *argv[]) {
     g.addEdge(0,1);
     g.addEdge(1,2);
     g.addEdge(2,3);
+    g.addEdge(3,0);
     g.addEdge(3,4);
     g.addEdge(4,5);
+    g.addEdge(5,0);
     g.bfs(0);
     return 0;
 }
